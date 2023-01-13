5f09bb48b0409df348f6b4c5d2e961e384c3f783
Chuks Miracle <chuksmiracle10@gmail.com>
2023-01-13 05:17:03 +0000

    The strong coo

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -6,6 +6,6 @@ def safe_function(fct, *args):
     try:
         result = fct(args[0], args[1])
     except Exception as n:
-        print(f"Exception: {n}", file=sys.stderr)
+        printf("Exception: {n}", file=sys.stderr)
     else:
         return result
