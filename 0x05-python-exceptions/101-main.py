eeb0230f298f4ad0c02aaa12982f0756e08e50c3
Chuks Miracle <chuksmiracle10@gmail.com>
2023-01-13 05:40:51 +0000

    Trying

--- 0x05-python-exceptions/101-main.py
+++ 0x05-python-exceptions/101-main.py
@@ -2,29 +2,43 @@
 safe_function = __import__('101-safe_function').safe_function
 
 
+
+
+
 def my_div(a, b):
 
     return a / b
 
+
+
 result = safe_function(my_div, 10, 2)
 
-printf("result of my_div: {}".format(result))
+print("result of my_div: {}".format(result))
+
+
 
 result = safe_function(my_div, 10, 0)
 
-printf("result of my_div: {}".format(result))
+print("result of my_div: {}".format(result))
+
+
+
 
 
 def print_list(my_list, len):
-	i = 0
-	
-	while i < len:
 
-        	print(my_list[i])
+    i = 0
 
-        	i += 1
-	return len
+    while i < len:
+
+        print(my_list[i])
+
+        i += 1
+
+    return len
+
+
 
 result = safe_function(print_list, [1, 2, 3, 4], 10)
 
-printf("result of print_list: {}".format(result))
+print("result of print_list: {}".format(result))
